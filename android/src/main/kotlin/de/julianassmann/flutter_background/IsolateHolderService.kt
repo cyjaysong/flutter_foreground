b55276b6f477463413359ffcef8f762c9d127333
CyJaySong <29367599+cyjaysong@users.noreply.github.com>
2024-09-26 15:07:58 +0800

    Notification setWhen

--- android/src/main/kotlin/de/julianassmann/flutter_background/IsolateHolderService.kt
+++ android/src/main/kotlin/de/julianassmann/flutter_background/IsolateHolderService.kt
@@ -106,6 +106,7 @@ class IsolateHolderService : Service() {
         val imageId = resources.getIdentifier(FlutterBackgroundPlugin.notificationIconName, FlutterBackgroundPlugin.notificationIconDefType, packageName)
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setOngoing(true)
+            .setWhen(false)
             .setContentTitle(FlutterBackgroundPlugin.notificationTitle)
             .setContentText(FlutterBackgroundPlugin.notificationText)
             .setSmallIcon(imageId)
